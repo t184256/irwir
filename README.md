1431e32ab8657fed1fbce49abcf2442175b86266
Alexander Sosedkin <monk@unboiled.info>
2018-07-22 12:13:41 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# irwir: an input wrapper
+
+This is my first attempt at programming in Rust,
+a generic input remapper program for Linux.
+
+This initial version is pretty much a no-op.
+
+Needs read access to input devices and write access to `/dev/uinput`.
+
+Possible future goals:
+
+* Multidevice remapping
+* TOML configuration files
+* Automatically picking up the desired uinput device capabilities
+* Multilayer remapping (e.g. to physical position first)
+* A stack of major modes (with only the topmost transformation being active)
+* A stack of minor modes (with all of the transformations being active)
+* Precomputing all transformations into a single hashmap
+* gluon scripting
+
+TODO:
+
+* Drop evdev-rs dependency (https://github.com/arcnmx/input-linux-rs/issues/1)
---
+++ src/main.rs
@@ -0,0 +1,55 @@
+extern crate evdev_rs;
+extern crate input_linux;
+extern crate ref_slice;
+use std::fs::File;
+use input_linux::{EvdevHandle, UInputHandle};
+use input_linux::{InputEvent, Event, KeyEvent};
+
+// I don't know a better way to create events from strings
+// https://github.com/arcnmx/input-linux-rs/issues/1
+fn key_from_name(s: &str) -> input_linux::Key {
+    let ec = evdev_rs::enums::EventCode::from_str(&evdev_rs::enums::EventType::EV_KEY, s).unwrap();
+    let code = evdev_rs::util::event_code_to_int(&ec).1;
+    input_linux::Key::from_code(code as u16).unwrap()
+}
+
+
+fn main() {
+    let device_path = String::from("/dev/input/by-path/platform-i8042-serio-0-event-kbd");
+    println!("{}", device_path);
+
+    let abort_key_name = "KEY_PAUSE";
+    let abort_key = key_from_name(abort_key_name);
+
+    let in_fd = File::open(device_path).unwrap();
+    let in_dev = EvdevHandle::new(&in_fd);
+    in_dev.grab(true).unwrap();
+
+    let ui_fd = File::create("/dev/uinput").unwrap();
+    let ui_dev = UInputHandle::new(&ui_fd);
+    ui_dev.set_evbit(input_linux::EventKind::Key).unwrap();
+    ui_dev.set_keybit(input_linux::Key::KeyA).unwrap();
+    ui_dev.set_keybit(input_linux::Key::KeyB).unwrap();
+    ui_dev.create(&input_linux::InputId::default(), b"test", 0, &[]).unwrap();
+
+    println!("Events:");
+    loop {
+        // TODO: am I overcomplicating things?
+        let mut input_event = unsafe { std::mem::zeroed() };
+        let a = in_dev.read(ref_slice::ref_slice_mut(&mut input_event));
+        if let Ok(1) = a {
+            let ev = *InputEvent::from_raw(&input_event).unwrap();
+            let ev = Event::new(ev).unwrap();
+            match ev {
+                Event::Key(KeyEvent { key, value, .. }) if key == abort_key && value == 0 => break,
+                _ => {
+                    //println!("{:?}", ev);
+                    ui_dev.write(ref_slice::ref_slice(&ev.as_ref().as_raw())).unwrap();
+                }
+            }
+        } else {
+            // TODO: more idiomatic error handling? match costs indentation
+            panic!();
+        }
+    }
+}
